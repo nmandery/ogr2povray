8efbf352a1c7edd4e50189bb6fcf604fbb320b1f
nico <nico@salix.(none)>
2009-02-15 13:58:46 +0100

    simplified documentation structure

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,7 @@ import sys, os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('../'))
 
 import ogr2povray
 
@@ -31,10 +31,10 @@ extensions = []
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = '.txt'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'readme'
 
 # General substitutions.
 project = u'ogr2povray'
